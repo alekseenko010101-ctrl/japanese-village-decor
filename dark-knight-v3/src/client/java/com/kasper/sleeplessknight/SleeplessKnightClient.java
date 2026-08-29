186023e931ed1c547757ac868f5809283bdebef3
alekseenko010101-ctrl <alekseenko010101@gmail.com>
2026-08-29 14:45:24 +0400

    Register Dark Knight battle music client tick

--- dark-knight-v3/src/client/java/com/kasper/sleeplessknight/SleeplessKnightClient.java
+++ dark-knight-v3/src/client/java/com/kasper/sleeplessknight/SleeplessKnightClient.java
@@ -1,11 +1,13 @@
 package com.kasper.sleeplessknight;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 
 public final class SleeplessKnightClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         EntityRendererRegistry.register(ModEntities.DARK_KNIGHT, DarkKnightRenderer::new);
+        ClientTickEvents.END_CLIENT_TICK.register(DarkKnightMusicManager::tick);
     }
 }
