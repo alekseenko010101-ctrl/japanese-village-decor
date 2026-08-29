89219314fd0ae4408d1ed009c0b4e491834d5603
alekseenko010101-ctrl <alekseenko010101@gmail.com>
2026-08-29 13:23:13 +0400

    Simplify Dark Knight client registration

--- dark-knight/src/client/java/com/kasper/sleeplessknight/SleeplessKnightClient.java
+++ dark-knight/src/client/java/com/kasper/sleeplessknight/SleeplessKnightClient.java
@@ -1,20 +1,11 @@
 package com.kasper.sleeplessknight;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.minecraft.client.model.geom.ModelLayerLocation;
-import net.minecraft.resources.Identifier;
 
 public final class SleeplessKnightClient implements ClientModInitializer {
-    public static final ModelLayerLocation DARK_KNIGHT_LAYER = new ModelLayerLocation(
-            Identifier.fromNamespaceAndPath(SleeplessKnight.MOD_ID, "dark_knight"),
-            "main"
-    );
-
     @Override
     public void onInitializeClient() {
-        EntityModelLayerRegistry.registerModelLayer(DARK_KNIGHT_LAYER, DarkKnightModel::createBodyLayer);
         EntityRendererRegistry.register(ModEntities.DARK_KNIGHT, DarkKnightRenderer::new);
     }
 }
