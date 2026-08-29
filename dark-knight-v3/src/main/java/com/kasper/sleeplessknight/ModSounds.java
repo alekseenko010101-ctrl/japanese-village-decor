9186a6130fe450b43c1284d0deab740cdd0c83a9
alekseenko010101-ctrl <alekseenko010101@gmail.com>
2026-08-29 14:45:41 +0400

    Register Dark Knight battle music sound event

--- dark-knight-v3/src/main/java/com/kasper/sleeplessknight/ModSounds.java
+++ dark-knight-v3/src/main/java/com/kasper/sleeplessknight/ModSounds.java
@@ -9,11 +9,20 @@ public final class ModSounds {
     private static final Identifier APPEAR_ID = Identifier.fromNamespaceAndPath(
             SleeplessKnight.MOD_ID, "dark_knight_appear"
     );
+    private static final Identifier BATTLE_MUSIC_ID = Identifier.fromNamespaceAndPath(
+            SleeplessKnight.MOD_ID, "dark_knight_battle_music"
+    );
 
     public static final SoundEvent DARK_KNIGHT_APPEAR = Registry.register(
             BuiltInRegistries.SOUND_EVENT,
             APPEAR_ID,
             SoundEvent.createVariableRangeEvent(APPEAR_ID)
+    );
+
+    public static final SoundEvent DARK_KNIGHT_BATTLE_MUSIC = Registry.register(
+            BuiltInRegistries.SOUND_EVENT,
+            BATTLE_MUSIC_ID,
+            SoundEvent.createVariableRangeEvent(BATTLE_MUSIC_ID)
     );
 
     private ModSounds() {}
